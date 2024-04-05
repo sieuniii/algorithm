b88a857415420f0c2fedf26f0202a90f1102e970
신시은 / Shin Sieun <100492603+sieuniii@users.noreply.github.com>
2024-04-05 17:00:20 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 224 ms, Memory: 14904 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        for(int i=1; i<=N; i++){
+            for (int j=1; j<= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
