d5f8caa191ca361d0f46fcb033f33aa2547c7ce0
신시은 / Shin Sieun <100492603+sieuniii@users.noreply.github.com>
2024-04-05 16:47:24 +0900

    [Bronze II] Title: 단어의 개수, Time: 260 ms, Memory: 20024 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int count = st.countTokens();
+        System.out.println(count);
+
+    }
+}
