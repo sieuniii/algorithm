350e1c4d13180f14614a8edb212d04bde84aeb1a
신시은 / Shin Sieun <100492603+sieuniii@users.noreply.github.com>
2024-04-05 16:32:00 +0900

    [Bronze V] Title: A/B, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double A = Double.parseDouble(st.nextToken());
+        double B = Double.parseDouble(st.nextToken());
+        System.out.println(A / B);
+
+    }
+}
